2b64647764bd452343169ee960d6b40e5dca3408
Jsesan <jsesan00@gmail.com>
2021-05-19 15:24:33 -0300

    APP

---
+++ src/main/java/com/pluralsight/conferencedemo/ConferenceDemoApplication.java
@@ -0,0 +1,15 @@
+package com.pluralsight.conferencedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//This told us this is a spring boot app
+@SpringBootApplication
+public class ConferenceDemoApplication {
+
+	//
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceDemoApplication.class, args);
+	}
+
+}
